3e7c5bcb7efbe7dab083aa8a0e969adee2352bd7
callmeray <c.ray695@gmail.com>
2022-04-18 14:33:30 +0800

    add multi-process in binary_ir_sim.py

--- tools/binary_ir_sim.py
+++ tools/binary_ir_sim.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn
 from PIL import Image
 import time
+import multiprocessing
 
 parser = argparse.ArgumentParser(description="Extract LCN IR pattern from IR images")
 parser.add_argument(
@@ -23,6 +24,7 @@ parser.add_argument(
     required=True,
 )
 parser.add_argument("-p", "--patch", type=int, required=True)
+parser.add_argument("--mp", type=int, default=1, help="multi-process")
 args = parser.parse_args()
 
 
@@ -41,23 +43,38 @@ def get_smoothed_ir_pattern2(img_ir: np.array, img: np.array, ks=11, threshold=0
     return ir
 
 
-def main():
-    with open(args.split_file, "r") as f:
-        prefix = [line.strip() for line in f]
-    num = len(prefix)
-
+def sub_main(prefix_list):
+    n = len(prefix_list)
     start = time.time()
-    for idx in range(num):
+    for idx in range(n):
         for direction in ["irL", "irR"]:
-            p = prefix[idx]
+            p = prefix_list[idx]
             f0 = os.path.join(args.data_folder, p, f"0128_{direction}_kuafu_half.png")
             f6 = os.path.join(args.data_folder, p, f"0128_{direction}_kuafu_half_no_ir.png")
             img_0 = np.array(Image.open(f0).convert(mode="L"))
             img_6 = np.array(Image.open(f6).convert(mode="L"))
 
-            print(f"Generating {p} binary sim {direction} pattern {idx}/{num} time: {time.time() - start:.2f}s")
+            print(f"Generating {p} binary sim {direction} pattern {idx}/{n} time: {time.time() - start:.2f}s")
             binary_pattern = get_smoothed_ir_pattern2(img_6, img_0, ks=args.patch)
             cv2.imwrite(os.path.join(args.data_folder, p, f"0128_{direction}_bin_ps{args.patch}.png"), binary_pattern)
+
+
+def main():
+    print("Multi-processing: ", args.mp)
+    with open(args.split_file, "r") as f:
+        prefix = [line.strip() for line in f]
+    num = len(prefix)
+    assert num % args.mp == 0
+    l = num // args.mp
+
+    p_list = []
+    for i in range(args.mp):
+        p = multiprocessing.Process(target=sub_main, args=(prefix[i * l : (i + 1) * l],))
+        p.start()
+        p_list.append(p)
+
+    for p in p_list:
+        p.join()
 
 
 if __name__ == "__main__":
