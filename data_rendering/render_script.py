dbd6c0c8c93fbe022146e1e573bade7d7297fc61
callmeray <c.ray695@gmail.com>
2022-04-25 11:27:41 +0800

    add logger in render_script.py

--- data_rendering/render_script.py
+++ data_rendering/render_script.py
@@ -1,9 +1,11 @@
 import os
 import os.path as osp
 import time
+import sys
 
 import numpy as np
 from path import Path
+from loguru import logger
 
 CUR_DIR = os.path.dirname(__file__)
 REPO_ROOT = os.path.abspath(osp.join(osp.dirname(__file__), ".."))
@@ -29,6 +31,26 @@ if __name__ == "__main__":
     target_root = args.target_root
     Path(target_root).makedirs_p()
 
+    timestamp = time.strftime("%y-%m-%d_%H-%M-%S")
+    name = "render_" + target_root.split("/")[-1]
+    filename = f"log.render.sub{args.sub:02d}.tot{args.total}.{timestamp}.txt"
+    # set up logger
+    logger.remove()
+    fmt = (
+        f"<green>{{time:YYYY-MM-DD HH:mm:ss.SSS}}</green> | "
+        f"<cyan>{name}</cyan> | "
+        f"<lvl>{{level}}</lvl> | "
+        f"<lvl>{{message}}</lvl>"
+    )
+
+    # logger to file
+    log_file = Path(target_root) / filename
+    logger.add(log_file, format=fmt)
+
+    # logger to std stream
+    logger.add(sys.stdout, format=fmt)
+    logger.info(f"Args: {args}")
+
     if args.primitives:
         total_scene = 2000
         scene_names = np.arange(total_scene)
@@ -52,14 +74,14 @@ if __name__ == "__main__":
                 sub_scene_list.append(f"0-{s}")
                 sub_scene_list.append(f"1-{s}")
 
-    print(f"Generating {len(sub_scene_list)} scenes from {sub_scene_list[0]} to {sub_scene_list[-1]}")
+    logger.info(f"Generating {len(sub_scene_list)} scenes from {sub_scene_list[0]} to {sub_scene_list[-1]}")
     start_time = time.time()
 
     for sc in sub_scene_list:
         if osp.exists(osp.join(target_root, f"{sc}-{num_view-1}/depthR_colored.png")):
-            print(f"Skip scene {sc}")
+            logger.info(f"Skip scene {sc}")
             continue
-        print(f"Generating scene {sc}")
+        logger.info(f"Generating scene {sc}")
         o = (
             f"{python_path} {render_py_path} --repo-root {repo_root}"
             f" --target-root {target_root} --spp {spp} --nv {num_view} -s {sc}"
@@ -73,4 +95,4 @@ if __name__ == "__main__":
         if args.primitives_v2:
             o += " --primitives-v2"
         os.system(o)
-        print(f"time: {time.time() - start_time}")
+        logger.info(f"time: {time.time() - start_time}")
