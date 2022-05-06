8a70cd4453e18cd55f8233f752d3faaf7a7ad716
callmeray <c.ray695@gmail.com>
2022-05-06 16:35:45 +0800

    minor

--- data_rendering/utils/render_utils.py
+++ data_rendering/utils/render_utils.py
@@ -40,15 +40,15 @@ TEXTURE_SQ_LIST = "/messytable-slow/mini-imagenet-tools/mini_imagenet_square/lis
 ENV_MAP_FOLDER = "/messytable-slow/mini-imagenet-tools/rand_env/"
 ENV_MAP_LIST = "/messytable-slow/mini-imagenet-tools/rand_env/list.txt"
 
-TEXTURE_FOLDER = "/media/DATA/LINUX_DATA/activezero2/datasets/mini_imagenet/"
-TEXTURE_LIST = "/media/DATA/LINUX_DATA/activezero2/datasets/mini_imagenet_list.txt"
-OBJECT_DIR = "/home/rayu/Projects/ICCV2021_Diagnosis/ocrtoc_materials/models/"
-OBJECT_CSV_PATH = "/home/rayu/Projects/ICCV2021_Diagnosis/ocrtoc_materials/objects.csv"
-SCENE_DIR = "/home/rayu/Projects/ICCV2021_Diagnosis/ocrtoc_materials/scenes"
-TEXTURE_SQ_FOLDER = "/media/DATA/LINUX_DATA/activezero2/datasets/mini_imagenet_square/"
-TEXTURE_SQ_LIST = "/media/DATA/LINUX_DATA/activezero2/datasets/mini_imagenet_square/list.txt"
-ENV_MAP_FOLDER = "/media/DATA/LINUX_DATA/activezero2/datasets/rand_env/"
-ENV_MAP_LIST = "/media/DATA/LINUX_DATA/activezero2/datasets/rand_env/list.txt"
+# TEXTURE_FOLDER = "/media/DATA/LINUX_DATA/activezero2/datasets/mini_imagenet/"
+# TEXTURE_LIST = "/media/DATA/LINUX_DATA/activezero2/datasets/mini_imagenet_list.txt"
+# OBJECT_DIR = "/home/rayu/Projects/ICCV2021_Diagnosis/ocrtoc_materials/models/"
+# OBJECT_CSV_PATH = "/home/rayu/Projects/ICCV2021_Diagnosis/ocrtoc_materials/objects.csv"
+# SCENE_DIR = "/home/rayu/Projects/ICCV2021_Diagnosis/ocrtoc_materials/scenes"
+# TEXTURE_SQ_FOLDER = "/media/DATA/LINUX_DATA/activezero2/datasets/mini_imagenet_square/"
+# TEXTURE_SQ_LIST = "/media/DATA/LINUX_DATA/activezero2/datasets/mini_imagenet_square/list.txt"
+# ENV_MAP_FOLDER = "/media/DATA/LINUX_DATA/activezero2/datasets/rand_env/"
+# ENV_MAP_LIST = "/media/DATA/LINUX_DATA/activezero2/datasets/rand_env/list.txt"
 
 def parse_csv(filename):
     """Parse the CSV file to acquire the information of objects used in TOC.
